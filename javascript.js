1b403c01f0f94506ebb2bc0d6b109e3d85fd31b7
Dan Kimberley <danielkimberley@gmail.com>
2024-03-27 00:13:08 +0000

    working timeline and functional markers, flipping not right yet

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
 // const moveButton = document.querySelector('button')
 // moveButton.addEventListener('click', moveCircle)
 
+const markers = []
+
+function checkDirection(xPos) {
+    let conflicts = 0
+    markers.forEach((marker) => {
+        if(Math.abs(marker.getBoundingClientRect().x - xPos) < 100 ) {
+            console.log(`True: ${Math.abs(marker.getBoundingClientRect().x - xPos)}`)
+            conflicts ++
+        }
+    })
+    return (conflicts >= 1)
+}
+
 function convertPosToYear(xPos) {
     const startDiv = document.getElementById('starting-year')
     let start = Math.round(startDiv.getBoundingClientRect().x)
@@ -14,13 +27,12 @@ function convertPosToYear(xPos) {
 
     percentage = (xPos - start) / (end - start)
 
-    if (percentage >= 0) {
-        return (Math.round(percentage * range) + "AD")
-    } else {
-        return (Math.round(percentage * range) + "BC")
-    }
+    let result = Math.round((percentage * range) + Number(startVal))
+
+    return result
     
 }
+
 
 handleMouseMove = (event) => {
     const x = event.clientX;
@@ -35,36 +47,51 @@ handleMouseStop = (event) => {
     console.log('mousestop');
     const ref = document.getElementById('circle');
     ref.classList.remove('moving');
-    ref.classList.remove('clicked')
-    console.log('stopped');
+    ref.classList.remove('clicked');
 }
 
 handleMouseClick = (event) => {
     const ref = document.getElementById('circle');
     ref.classList.add('clicked');
+    xPos = event.clientX
+    let flip = checkDirection(xPos)
+    console.log(checkDirection(xPos))
 
     const input = document.querySelector('input') 
     const inputText = input.value
+    input.value = ''
+    console.log(inputText) 
 
     const newBox = document.createElement('div')
-    newBox.classList.add('marker')
+    if (flip == false) {
+        newBox.classList.add('marker')
+    } else {
+        newBox.classList.add('marker')
+        newBox.classList.add('marker-flipped')
+    }
+    
     
     const boxHeader = document.createElement('h1')
-    boxHeader.textContent = convertPosToYear(event.clientX)
+    boxHeader.textContent = convertPosToYear(xPos)
     newBox.appendChild(boxHeader)
 
     const boxIcon = document.createElement('img')
     boxIcon.src = 'x.svg'
+    boxIcon.addEventListener('click', () => {
+        boxIcon.parentNode.remove()
+        })
     newBox.appendChild(boxIcon)
 
     const boxDetail = document.createElement('p')
-    boxDetail.textContent = input.value
+    boxDetail.textContent = inputText
     newBox.appendChild(boxDetail)
 
     const container = document.getElementById('line');
     newBox.style.position = 'absolute';
-    newBox.style.left = event.clientX + 'px';
+    newBox.style.left = xPos + 'px';
     container.appendChild(newBox)
+
+    markers.push(newBox)
 
 }
 
@@ -84,6 +111,24 @@ window.addEventListener("mousedown",function(){
 
 document.addEventListener('mousemove', handleMouseMove);
 
-document.addEventListener('mousedown', handleMouseClick)
+document.addEventListener('mousedown', (e) => {
+    if((e.target !== e.currentTarget && e.target == document.querySelector('body')) || e.target == document.getElementById('circle')) {
+        handleMouseClick(e)
+        console.log(e.target)
+    }
+})
 
 const deleteButton = document.get
+
+function pdf() {
+    console.log('Saving...')
+    const element = document.querySelector('body')
+    let opt = {
+        margin:       1,
+        filename:     'myfile.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 0.5 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape' }
+      };
+      html2pdf(element, opt)
+}
